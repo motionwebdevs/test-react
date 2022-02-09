420b9aa752137d96315a981aad987049f0924f94
thedastan <dastanshermatov@gmail.com>
2022-02-09 12:19:41 +0600

    initial

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+const Header = () => {
+    return (
+        <header id="header">
+            <div className="container">
+                <div className="header">
+                    <a href="#" className="header__logo">
+                        logo.
+                    </a>
+                    <nav className="header__nav">
+                        <a href="#" className="header__nav--item">Home</a>
+                        <a href="#" className="header__nav--item">Home</a>
+                        <a href="#" className="header__nav--item">Home</a>
+                        <a href="#" className="header__nav--item">Home</a>
+                        <a href="#" className="header__nav--item">Home</a>
+                        <a href="#" className="header__nav--item">Home</a>
+                        <a href="#" className="header__nav--item">Home</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+function Hero() {
+    return (
+        <section id="hero">
+            <div className="container">
+                <div className="hero">
+                    <h1 className="hero__title">Delecious food zone</h1>
+                    <p className="hero__subtitle">There are many variations of passages of Lorem Ipsum <br/> available,
+                        but the majority have suffered alteration in <br/> some form, by injected humour</p>
+                    <button className="hero__btn">Book Your Table</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
