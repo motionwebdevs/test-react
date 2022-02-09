b6fee366a4ad3dc0059a55114c854525d9dad929
thedastan <dastanshermatov@gmail.com>
2022-02-09 12:38:48 +0600

    edit href

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,17 @@ const Header = () => {
         <header id="header">
             <div className="container">
                 <div className="header">
-                    <a href={"#"} className="header__logo">
+                    <a href={"#header"} className="header__logo">
                         logo.
                     </a>
                     <nav className="header__nav">
-                        <a href={"#"} className="header__nav--item">Home</a>
-                        <a href={"#"} className="header__nav--item">Home</a>
-                        <a href={"#"} className="header__nav--item">Home</a>
-                        <a href={"#"} className="header__nav--item">Home</a>
-                        <a href={"#"} className="header__nav--item">Home</a>
-                        <a href={"#"} className="header__nav--item">Home</a>
-                        <a href={"#"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
+                        <a href={"#header"} className="header__nav--item">Home</a>
                     </nav>
                 </div>
             </div>
